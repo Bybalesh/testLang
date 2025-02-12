8e6f342c6a107c2ff9fbcf010601a5b29d201575
Nik <bybalesh234@gmail.com>
2025-02-12 19:27:32 +0600

    Update 1.s

--- lang/1.s
+++ lang/1.s
@@ -1,24 +1,23 @@
 .global main  @ Объявляем символ 'main' глобальным (видимым для линковщика)
 
-.text          @ Указываем, что далее идет секция кода
-
-main:
-  @ Сохраняем регистр link register (LR) в стеке
-  push {lr}
-
-  @ Загружаем адрес строки "Hello, World!\n" в регистр r0
-  ldr r0, =hello_string
-
-  @ Вызываем функцию puts для вывода строки на экран
-  bl puts
-
-  @ Возвращаем 0 (код успешного завершения)
-  mov r0, #0
-
-  @ Восстанавливаем регистр link register (LR) из стека и возвращаемся
-  pop {pc}
-
-.data          @ Указываем, что далее идет секция данных
-
 hello_string:
   .asciz "Hello, World!\n"  @ Определяем строку "Hello, World!\n" с завершающим нулевым байтом
+```assembly
+.section .data
+msg:
+    .ascii "Hello, World!\n"
+len = . - msg
+
+.section .text
+.global _start
+_start:
+    mov $1, %rax        # Системный вызов write (номер 1)
+    mov $1, %rdi        # Описатель файла stdout (1)
+    mov $msg, %rsi      # Адрес строки
+    mov $len, %rdx      # Длина строки
+    syscall             # Вызов системного вызова
+
+    mov $60, %rax       # Системный вызов exit (номер 60)
+    xor %rdi, %rdi      # Код завершения 0
+    syscall             # Вызов системного вызова
+```
